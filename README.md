6132c783f0636c9a356637d5be7629e65613298e
Third Santor <tlsantor@gmail.com>
2013-07-10 10:44:31 -0400

    Refactored generated code

--- README.md
+++ README.md
@@ -22,6 +22,12 @@ See it in action!
 http://codepen.io/third/details/IGCfo
 
 
+See what I made out of it
+-------------------------
+
+http://youtu.be/FFTpXe6S0P8
+
+
 Motivation
 ----------
 
@@ -31,3 +37,4 @@ I got an Arduino UNO when I attended JSConf 2013 last May. I have seen very cool
 Lately, I see a lot of topics about robots on my Twitter feed so I decided to pull my Arduino e-book and started reading. The first coding topic was about "blinking LED". The code was straight forward - assign a digital pin as an output then turn on and turn off the digital pin after a second. Whoa, awesome! Then I thought I should make a little bit complex blinking pattern. I thought about the honk sound that me and my cousins used to make. I was trying to find a reference and it looks like the sound that I am referring is similar to an [SMS tone](http://www.youtube.com/watch?v=scFR4sYnVDc). I wanted to translate that sound into blinks such that I can play that sound in my head while watching my Arduino blink. I was trying to compose that directly into my Arduino IDE. I was only on the first couple of blinks and I find it hard to recreate. One thing I notice though is that I started moving my arms up and down like an orchestra conductor in trying to get the exact delay in turning on and turning off the LED. Then, Eureka! It came to me that I can do the same with the help of a keyboard and let the computer calculate the timings for me so I quickly created Arduino Blinker using Javascript, HTML and CSS. 
 
 I know that this is a very simple tool and not a rocket science. Let's face it, it's only blinking lights and does nothing special. You might find it boring after several tries. What I'm trying to show here is that, you can always create simple tools that will help you achieve complex things. And most of the time, I like it better when it starts to get boring because that is the time when I started thinking again for the next level.
+
--- js/led-recorder.js
+++ js/led-recorder.js
@@ -1,205 +1,214 @@
 var states = {
-	RESET: 0,
-	START: 1,
-	STOP: 2
+  RESET: 0,
+  START: 1,
+  STOP: 2
 }
 
 var LedRecorder = function(options) {
-	this.blinks	= [];
-	this.startTime = null;
-	this.state = states.RESET;
-	this.keyIsDown = false;
-	this.isRoundDown = false;
+  this.blinks  = [];
+  this.startTime = null;
+  this.state = states.RESET;
+  this.keyIsDown = false;
+  this.isRoundDown = false;
 
-	this.btnRecorder = options.btnRecorder;
-	this.btnPreview = options.btnPreview;
-	this.radRoundDown = options.radRoundDown;
-	this.divLed = options.divLed;
-	this.txtResult = options.txtResult;
+  this.btnRecorder = options.btnRecorder;
+  this.btnPreview = options.btnPreview;
+  this.radRoundDown = options.radRoundDown;
+  this.divLed = options.divLed;
+  this.txtResult = options.txtResult;
 
-	this.init();
+  this.init();
 }
 
 LedRecorder.prototype = {
-	init: function () {
-		var oThis = this;
+  init: function () {
+    var oThis = this;
 
 
-		oThis.reset();
+    oThis.reset();
 
 
-		//bind events to buttons
-		oThis.btnRecorder.onclick = function() {
-			oThis.toggleRecorder();
-		}
+    //bind events to buttons
+    oThis.btnRecorder.onclick = function() {
+      oThis.toggleRecorder();
+    }
 
-		oThis.btnPreview.onclick = function() {
-			oThis.replay();
-		}			
-	},
+    oThis.btnPreview.onclick = function() {
+      oThis.replay();
+    }      
+  },
 
-	reset: function () {
-		var txtResult = this.txtResult;
-		var btnPreview = this.btnPreview;
+  reset: function () {
+    var txtResult = this.txtResult;
+    var btnPreview = this.btnPreview;
 
-		if(txtResult) {
-			txtResult.value = '';
-		}
+    if(txtResult) {
+      txtResult.value = '';
+    }
 
-		this.togglePreview();
+    this.togglePreview();
 
-		this.blinks = [];
-		this.state = states.RESET;
-	},
+    this.blinks = [];
+    this.state = states.RESET;
+  },
 
-	startTimer: function() {
-		this.startTime = (new Date()).getTime();
-	},
+  startTimer: function() {
+    this.startTime = (new Date()).getTime();
+  },
 
-	endTimer: function() {
-		var endTime = (new Date()).getTime()
-		var elapsed = endTime - this.startTime;
+  endTimer: function() {
+    var endTime = (new Date()).getTime()
+    var elapsed = endTime - this.startTime;
 
-		//console.log('orig value = ' + elapsed)
+    //console.log('orig value = ' + elapsed)
 
-		if(this.isRoundDown) {
-			elapsed = Math.floor(elapsed / 10)*10;
-		}
-		else {
-			elapsed = Math.round(elapsed / 10)*10;
-		}
+    if(this.isRoundDown) {
+      elapsed = Math.floor(elapsed / 10)*10;
+    }
+    else {
+      elapsed = Math.round(elapsed / 10)*10;
+    }
 
-		//console.log('new value = ' + elapsed)
+    //console.log('new value = ' + elapsed)
 
-		this.blinks.push(elapsed);
-	},
+    this.blinks.push(elapsed);
+  },
 
-	toggleRecorder: function() {
-		var btnRecorder = this.btnRecorder;
-		
-		this.togglePreview();
+  toggleRecorder: function() {
+    var btnRecorder = this.btnRecorder;
+    
+    this.togglePreview();
 
-		if(btnRecorder) {
-			var state = this.state;
+    if(btnRecorder) {
+      var state = this.state;
 
-			if(state === states.RESET) {
-				//move to START
-				this.state = states.START;
-				btnRecorder.value = 'Stop Recording'					
-			}
-			else if(state === states.START) {
-				//move to STOP
-				this.state = states.STOP;
-				btnRecorder.value = 'Reset Recording';
+      if(state === states.RESET) {
+        //move to START
+        this.state = states.START;
+        btnRecorder.value = 'Stop Recording'          
+      }
+      else if(state === states.START) {
+        //move to STOP
+        this.state = states.STOP;
+        btnRecorder.value = 'Reset Recording';
 
-				this.endTimer();
+        this.endTimer();
 
-				this.displayResult();
-			}
-			else if(state === states.STOP) {
-				this.state = states.RESET;
+        this.displayResult();
+      }
+      else if(state === states.STOP) {
+        this.state = states.RESET;
 
-				btnRecorder.value = 'Start Recording';
+        btnRecorder.value = 'Start Recording';
 
-				this.init();
-			}
-		}
-	},
+        this.init();
+      }
+    }
+  },
 
-	togglePreview: function() {
-		var btnPreview = this.btnPreview;
-		btnPreview.style.visibility = (this.state === states.START ? 'visible' : 'hidden');
-	},
+  togglePreview: function() {
+    var btnPreview = this.btnPreview;
+    btnPreview.style.visibility = (this.state === states.START ? 'visible' : 'hidden');
+  },
 
-	ledOn: function() {
-		var state = this.state;
+  ledOn: function() {
+    var state = this.state;
 
-		if(state === states.START && this.keyIsDown === false) {
+    if(state === states.START && this.keyIsDown === false) {
 
-			if(this.blinks.length > 0) {
-				this.endTimer();
-			}
+      if(this.blinks.length > 0) {
+        this.endTimer();
+      }
 
-			this.isRoundDown = this.radRoundDown.checked;
+      this.isRoundDown = this.radRoundDown.checked;
 
-			this.startTimer();
+      this.startTimer();
 
-			this.toggleLed(true);
+      this.toggleLed(true);
 
-			this.keyIsDown = true;
-		}
-	},
+      this.keyIsDown = true;
+    }
+  },
 
-	ledOff: function() {
-		var state = this.state;
+  ledOff: function() {
+    var state = this.state;
 
-		if(state === states.START) {				
-			this.endTimer();
-			this.startTimer();
-			
-			this.toggleLed(false);
+    if(state === states.START) {        
+      this.endTimer();
+      this.startTimer();
+      
+      this.toggleLed(false);
 
-			this.keyIsDown = false;
-		}
-	},
+      this.keyIsDown = false;
+    }
+  },
 
-	displayResult: function() {
-		var txtResult = this.txtResult;
+  displayResult: function() {
+    var txtResult = this.txtResult;
 
-		if(txtResult) {
-			var result = [];
+    if(txtResult) {
+      var result = [];
 
-			result.push('void setup() {');
-			result.push('  pinMode(13, OUTPUT);');
-			result.push('}');
-			result.push('\t');
-
-			result.push('void loop() {');
+      var blinks = this.blinks;
+      var count = blinks.length;
 
 
-			for(var i=0,l=this.blinks.length;i<l;i++) {
-				var blink = this.blinks[i];
-
-				result.push('  digitalWrite(13, ' + (i%2===0 ? 'HIGH' : 'LOW') + ');');
-					result.push('  delay(' + blink + ');');
-			}
-
-			result.push('}')
-
-			txtResult.value = result.join('\n');
-		}
-	},
-
-	replay: function() {
-		var oThis = this;
-		var counter = 0;
-
-		function processItem(arr) {
-			if(arr.length > 0) {
-				var delay = arr.shift();
-
-				var isOn = counter%2 === 0;
-				counter++;
-
-				oThis.toggleLed(isOn);
+      result.push('int delayArray[] = {' + blinks.join(',') + '};');
+      result.push('int count = ' + count + ';');
 
 
-				setTimeout(function() {
-					processItem(arr);
-				}, delay);					
-			}
-		}
+      result.push('void setup() {');
+      result.push('  pinMode(13, OUTPUT);');
+      result.push('}');
+      result.push('\t');
 
-		var blinks = oThis.blinks.slice(0);
+      result.push('void loop() {');
+      result.push('  for(count=0;count<' + count + ';count++){');
+      result.push('    if(count%2 == 0) {');
+      result.push('      digitalWrite(13, HIGH);');
+      result.push('    }');
+      result.push('    else {');
+      result.push('      digitalWrite(13, LOW);');
+      result.push('    }');
+      result.push('\t');      
+      result.push('    delay(delayArray[count]);');      
+      result.push('  }');
+      result.push('}')
 
-		processItem(blinks);
-	},
+      txtResult.value = result.join('\n');
+    }
+  },
 
-	toggleLed: function(isOn) {
-		var divLed = this.divLed;
-		if(divLed) {
-			divLed.className = divLed.className.replace(' led-on', '').replace(' led-off', '');
-			divLed.className += (isOn ? ' led-on' : ' led-off');
-		}
-	}
+  replay: function() {
+    var oThis = this;
+    var counter = 0;
+
+    function processItem(arr) {
+      if(arr.length > 0) {
+        var delay = arr.shift();
+
+        var isOn = counter%2 === 0;
+        counter++;
+
+        oThis.toggleLed(isOn);
+
+
+        setTimeout(function() {
+          processItem(arr);
+        }, delay);          
+      }
+    }
+
+    var blinks = oThis.blinks.slice(0);
+
+    processItem(blinks);
+  },
+
+  toggleLed: function(isOn) {
+    var divLed = this.divLed;
+    if(divLed) {
+      divLed.className = divLed.className.replace(' led-on', '').replace(' led-off', '');
+      divLed.className += (isOn ? ' led-on' : ' led-off');
+    }
+  }
 }
